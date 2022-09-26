e34a37c3fc71a598a1a42570c170b7b5d9d7f900
Karthik <56579449+karthik9728@users.noreply.github.com>
2022-09-27 02:02:20 +0530

    Product API Migration

--- MangoRestaurant/Mango.Services.ProductAPI/Program.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Program.cs
@@ -1,9 +1,22 @@
+using Mango.Services.ProductAPI.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+#region Database Configure
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(connectionString));
+
+#endregion
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
