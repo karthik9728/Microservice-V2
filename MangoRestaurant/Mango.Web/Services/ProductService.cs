2a3411041bc3b74879e9ab40fe40f9c1290b6893
Karthik <56579449+karthik9728@users.noreply.github.com>
2022-09-27 15:38:07 +0530

    Product Update UI

--- MangoRestaurant/Mango.Web/Services/ProductService.cs
+++ MangoRestaurant/Mango.Web/Services/ProductService.cs
@@ -28,7 +28,7 @@ namespace Mango.Web.Services
             return await this.SendAsync<T>(new ApiRequest()
             {
                 ApiType = SD.ApiType.DELETE,
-                Url = SD.ProductAPIBase + "/api/products" + id,
+                Url = SD.ProductAPIBase + "/api/products/" + id,
                 AccessToken = ""
             });
         }
@@ -48,7 +48,7 @@ namespace Mango.Web.Services
             return await this.SendAsync<T>(new ApiRequest()
             {
                 ApiType = SD.ApiType.GET,
-                Url = SD.ProductAPIBase + "/api/products" + id,
+                Url = SD.ProductAPIBase + "/api/products/" + id,
                 AccessToken = ""
             });
         }
