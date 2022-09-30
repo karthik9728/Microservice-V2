2879c35a190be451a25ad54a66e63f32eef851c1
Karthik <56579449+karthik9728@users.noreply.github.com>
2022-09-30 16:41:19 +0530

    Shopping Cart UI

---
+++ MangoRestaurant/Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        private readonly IProductService _productService;
+
+        public CartController(ICartService cartService, IProductService productService)
+        {
+            _cartService = cartService;
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> CartIndex()
+        {
+            return View(await LoadCartDtoBasedOnLoggedInUser());
+        }
+
+        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
+        {
+            var userId = User.Claims.Where(x => x.Type == "sub")?.FirstOrDefault()?.Value;
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var responce = await _cartService.GetCartByUserIdAsync<ResponceDto>(userId,accessToken);
+
+            CartDto cartDto = new();
+
+            if(responce != null && responce.IsSuccess)
+            {
+                cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(responce.Result));
+            }
+
+            if(cartDto.CartHeader != null)
+            {
+                foreach (var detail in cartDto.CartDetails)
+                {
+                    cartDto.CartHeader.OrderTotal += (detail.Product.Price * detail.Count);
+                }
+            }
+
+            return cartDto;
+        }
+    }
+}
